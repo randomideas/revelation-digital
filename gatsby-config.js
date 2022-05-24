d6275621e891ecc7f47ac0d223613366edfdf723
Kindlebit-php <kindlebit.php@gmail.com>
2022-05-24 13:37:53 +0530

    new test

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,16 +73,6 @@ module.exports = {
 		options: {
 		  siteUrl: 'https://revelation.digital',
 		},
-	},
-  {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `limelight`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
-        ],
-        display: 'swap'
-      }
-    }
+	}
   ],
 }
