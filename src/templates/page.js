8f5217600796cdb6895710ee912a2058ba0f6dd4
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-12-16 10:55:42 +0530

    Update page.js

--- src/templates/page.js
+++ src/templates/page.js
@@ -1005,7 +1005,7 @@ class Page extends Component {
 							<div className="section--normal-margin">
 										<div className="container">
 											<ReactWOW  animation='fadeInLeft'>
-											<h2 className={(prop.title_center == true)? ("text-bebas text-bebas--big text--pink features-title features-title--center") : ("text-bebas text-bebas--big text--pink features-title")}  > Our Services </h2>
+											<h2 className="text-bebas text-bebas--big text--pink features-title fadeInLeft"  > Our Services </h2>
 											</ReactWOW>
 
 									<div className="features-table features-table--3">
