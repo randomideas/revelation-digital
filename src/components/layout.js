4a34a24081851de897534962a16b331008f969ba
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-01-23 14:30:04 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ const TemplateWrapper = ({ children }) => (
 	<Helmet>
 	 {/* jquery */}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src={withPrefix("js/stellar.js")} ></script>
+	<script src={withPrefix("stellar.js")} ></script>
 	</Helmet>
     <Header/>
       <main className="main-content" >{children}</main>
