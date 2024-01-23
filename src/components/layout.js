d05330d178b386d1e1962a2f536d4e265552f61a
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-01-23 14:29:14 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,11 @@ import Footer from "./Common/Footer"
 const TemplateWrapper = ({ children }) => (
   <div>
      <Helmet title="RD" link={[{ rel: "shortcut icon", type: "image/png", href: `${favicon}` }]} />
-		
+	<Helmet>
+	 {/* jquery */}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src={withPrefix("js/stellar.js")} ></script>
+	</Helmet>
     <Header/>
       <main className="main-content" >{children}</main>
 	<Footer/>  
