c48030e193729e1dd313fe1452b1e2094944aaf5
randomideas <randomideas@gmail.com>
2025-07-24 16:07:03 +0800

    Navigation update

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,77 +1,107 @@
-import React, {Component} from 'react';
-import { Link} from 'gatsby'
-import axios from "axios"
-import SocialMenu from "./SocialMenu"
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import axios from "axios";
+import SocialMenu from "./SocialMenu";
 import Menubg from '../../images/menu_bg.mp4';
 import LazyLoad from 'react-lazyload';
+
 class Header extends Component {
-	
-	
-	state = {
-        pagedata: [],
-		showInfo: 0,
-		show: false
-		
-    };
-	
-	componentDidMount() {
-		
-		var url = "https://development.revelation.digital/wp-json/newrd/v1/rd_menu";
-		axios.get(url).then(e => {
-			this.setState({pagedata: e.data.ResponseData})
-			this.setState({showInfo: 1})
-		})
-		
-		
+  state = {
+    pagedata: [],
+    showInfo: 0,
+    show: false
+  };
+
+  componentDidMount() {
+    // TEMPORARY NAVIGATION FOR TESTING
+    // const tempMenu = [
+    //   { Slug: "", Name: "Home" },
+    //   { Slug: "https://github.com/jill-rd/revelation-digital", Name: "About Us" },
+    //   { Slug: "services", Name: "Services" },
+    //   { Slug: "contact", Name: "Contact" }
+    // ];
+
+    // this.setState({
+    //   pagedata: tempMenu,
+    //   showInfo: 1
+    // });
+
+    // --- ORIGINAL API CODE (commented for now) ---
+    const url = "https://development.revelation.digital/wp-json/newrd/v1/rd_menu";
+    axios.get(url).then(e => {
+      this.setState({ pagedata: e.data.ResponseData, showInfo: 1 });
+    });
+  }
+
+  render() {
+    let str_url = "";
+    const isBrowser = () => typeof window !== "undefined";
+    if (isBrowser()) {
+      str_url = window.location.pathname;
     }
-	
-	render() {
-		let str_url = "";
-		const isBrowser = () => typeof window !== "undefined"
-		if(isBrowser()){
-			str_url = window.location.pathname;
-			str_url.replace('/', '');
-		}
-		
 
-		return (
-			
-				<>
-			
-				<header className={this.state.show === false ? "header" : "header body-menu-open"} >
-					<a href="#" className="header-toggle" onClick={()=>{this.setState({show:!this.state.show})}} >menu toggle</a>
-					<div className="header-content">
-						<a href="/" className="logo">
-							<img width="356" height="58" src="https://development.revelation.digital/wp-content/uploads/2020/02/logo.svg" alt="Revelation Digital"/>
-						</a>
-					</div>
-				</header>
-					<div className="menu-wrapper" style={{ display: this.state.show === false ? "none" : "block" }} >
-					<div className="container">
-						<ul className="menu">
-						{(this.state.showInfo === 1) ? (
-							<>
-							{this.state.pagedata.map((prop,i) => {return (
-								<li key={"Menu"+i} className={("/"+prop.Slug === str_url) ? ("current-menu-item") : ("")} >
-							<a  href={"/"+prop.Slug} >{prop.Name}</a>
-								</li>
-							)})}
-							</>
-						) : ("")}
-						</ul>
-						<SocialMenu/>
-					</div>
-					<LazyLoad height={200} >
-					<video src={Menubg} autoPlay playsInline muted loop></video>
-    					</LazyLoad>	
-				</div>
+    return (
+      <>
+        <header className={this.state.show === false ? "header" : "header body-menu-open"}>
+          <a
+            href="#"
+            className="header-toggle"
+            onClick={() => {
+              this.setState({ show: !this.state.show });
+            }}
+          >
+            menu toggle
+          </a>
+          <div className="header-content">
+            <Link to="/" className="logo">
+              <img
+                width="356"
+                height="58"
+                src="https://development.revelation.digital/wp-content/uploads/2020/02/logo.svg"
+                alt="Revelation Digital"
+              />
+            </Link>
+          </div>
+        </header>
+        <div
+          className="menu-wrapper"
+          style={{ display: this.state.show === false ? "none" : "block" }}
+        >
+          <div className="container">
+            <ul className="menu">
+              {this.state.showInfo === 1 ? (
+                <>
+					{this.state.pagedata.map((prop, i) => {
+					const isExternal = /^https?:\/\//.test(prop.Slug);
+					const slugPath = isExternal ? prop.Slug : `/${prop.Slug.replace(/^\/+/, '')}`;
+					const isCurrent = !isExternal && slugPath === str_url ? "current-menu-item" : "";
 
-				</>
-			
-		)
-	}
+					return (
+						<li key={`Menu${i}`} className={isCurrent}>
+						{isExternal ? (
+							<a href={slugPath} target="_self" rel="noopener noreferrer">
+							{prop.Name}
+							</a>
+						) : (
+							<Link to={slugPath}>{prop.Name}</Link>
+						)}
+						</li>
+					);
+					})}
+                </>
+              ) : (
+                ""
+              )}
+            </ul>
+            <SocialMenu />
+          </div>
+          <LazyLoad height={200}>
+            <video src={Menubg} autoPlay playsInline muted loop></video>
+          </LazyLoad>
+        </div>
+      </>
+    );
+  }
 }
 
-
-
-export default Header
+export default Header;
--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -1,43 +1,59 @@
-import React, {Component} from 'react';
-import axios from "axios"
+import React, { Component } from 'react';
+import axios from "axios";
 import ReactTooltip from 'react-tooltip';
 
 class Nav extends Component {
-	
-	
-	state = {
-        pagedata: [],
-		showInfo: 0,
-		show: false
-		
-    };
-	componentDidMount() {
-		
-		var url = "https://development.revelation.digital/wp-json/newrd/v1/rd_nav";
-		axios.get(url).then(e => {
-			this.setState({pagedata: e.data.ResponseData})
-			this.setState({showInfo: 1})
-		})
+  state = {
+    pagedata: [],
+    showInfo: 0,
+    show: false
+  };
+
+  componentDidMount() {
+    const url = "https://development.revelation.digital/wp-json/newrd/v1/rd_nav";
+    axios.get(url).then(e => {
+      this.setState({ pagedata: e.data.ResponseData, showInfo: 1 });
+    });
+  }
+
+  render() {
+    const page = this.props.page;
+
+    // Determine current page path
+    let currentPath = "";
+    if (typeof window !== "undefined") {
+      currentPath = window.location.pathname;
     }
-	render() {
-		var page = this.props.page;
-		return (
-			<div className="solutions__nav">
-				{(this.state.showInfo === 1) ? (
-						<>
-						{this.state.pagedata.map((prop,i) => {return (
-							<>
-							<a data-tip={prop.tooltip} href={prop.title.toLowerCase()} className={(prop.title === page) ? ("is_active") : ("")} data-tippy-content={prop.tooltip}>{prop.title}.</a>
-							<ReactTooltip place="bottom" type="dark" effect="float"/>
-							</>
-						)})}
-					</>
-				) : ("")}
-			</div>
-		)
-	}
+
+    return (
+      <div className="solutions__nav">
+        {this.state.showInfo === 1 ? (
+          <>
+            {this.state.pagedata.map((prop, i) => {
+              // Normalize link path
+              const linkValue = prop.title.toLowerCase();
+              const isExternal = /^https?:\/\//.test(linkValue);
+              const slugPath = isExternal ? linkValue : `/${linkValue.replace(/^\/+/, '')}`;
+              const isCurrent = !isExternal && slugPath === currentPath ? "is_active" : "";
+
+              return (
+                <React.Fragment key={`Nav${i}`}>
+                  {isExternal ? (
+                    <a data-tip={prop.tooltip} href={slugPath} target="_self" rel="noopener noreferrer" className={isCurrent}>{prop.title}.</a>
+                  ) : (
+                    <a data-tip={prop.tooltip} href={slugPath} className={isCurrent}>{prop.title}.</a>
+                  )}
+                  <ReactTooltip place="bottom" type="dark" effect="float" />
+                </React.Fragment>
+              );
+            })}
+          </>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
-
-
-export default Nav
+export default Nav;
